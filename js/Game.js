9f043eec9bcb3cc43f82f8cf08c5337769de2e95
Sary Elmelegy <arch.selmelegy@gmail.com>
2018-11-13 15:45:33 +0200

    Game Class: gameOver, startGame and documentation

--- js/Game.js
+++ js/Game.js
@@ -1,33 +1,52 @@
 class Game{
     constructor(){
         this.missed = 0;
-        this.phrases = ['hello sary are you here'];
-        // this.phrases = ['hello', 'Sary', 'are', 'you', 'here'];
-        this.phrase = new Phrase(this.getRandomPhrase());
+        this.phrases = ['hello sary are you here',
+                        'My name is Sary',
+                        'Do you know my name',
+                        'This app is amazing',
+                        'OOP is not that difficult after all'];
+        this.phrase = this.getRandomPhrase;
     }
 
 
-    getRandomPhrase(){
+    /*
+     * Randomly retrieves one of the phrases stored in the phrases array.
+     * @return   {Object}   Phrase - The phrase that will be hidden.
+     */
+    get getRandomPhrase(){
         const randomNum = Math.floor(Math.random() * this.phrases.length);
-        return this.phrases[randomNum];
+        return new Phrase(this.phrases[randomNum]);
     }
 
 
+    /*
+     * Checks to see if the button clicked by the player matches a letter in the
+     * phrase.
+     */
     handleInteraction(){
+        const hiddenObject  = this.phrase;
+        const hiddenPhrase  = hiddenObject.phrase;
         $('#qwerty').on('click', e => {
-            const clickedLetter = e.target.textContent;
-            const hiddenObject  = this.phrase;
-            const hiddenPhrase  = hiddenObject.phrase;
-            if(hiddenObject.checkLetter(clickedLetter, hiddenPhrase)){
-                hiddenObject.showMatchedLetter(clickedLetter, hiddenPhrase);
-                this.checkForWin();
-            } else {
-                this.removeLife();
+            if(e.target.className === 'key'){
+                const clickedLetter = e.target.textContent;
+                if(hiddenObject.checkLetter(clickedLetter, hiddenPhrase)){
+                    hiddenObject.showMatchedLetter(clickedLetter, hiddenPhrase);
+                    if (this.checkForWin()) {
+                        this.gameOver();
+                    }
+                } else {
+                    this.removeLife();
+                }
             }
         })
     }
 
 
+    /*
+     * Removes a life, removes a heart from the board, and, if the player is out
+     * of lives, ends the game.
+     */
     removeLife(){
         const hearts    = $('#scoreboard ol').children();
         const lastHeart = $('#scoreboard ol').children().last();
@@ -35,11 +54,15 @@ class Game{
         if (livesNum > 1) {
             lastHeart.remove();
         } else {
+            lastHeart.remove();
             this.gameOver();
         }
     }
 
 
+    /*
+     * Checks to see if the player has selected all of the letters.
+     */
     checkForWin(){
         const letters = $('#phrase ul').children();
         for (let i = 0; i < letters.length; i++) {
@@ -51,5 +74,27 @@ class Game{
     }
 
 
-    gameOver(){}
+    /*
+     * Displays a message if the player wins and another message if he loses.
+     */
+    gameOver(){
+        const gameOverHeader = $('#game-over-message');
+        if (this.checkForWin()) {
+            $(gameOverHeader).text('Congratulations you won the Game Yaay ;)');
+        } else {
+            $(gameOverHeader).text('Sorry you lost, Try one more time');
+        }
+        $('#overlay').show();
+    }
+
+
+    /*
+     * Uses the randomly picked phrase to display it on the board
+     */
+    startGame(){
+        const phraseObj = this.phrase;
+        const phrase = phraseObj.phrase;
+        phraseObj.addPhraseToDisplay(phrase);
+        return phraseObj;
+    }
 }
--- js/app.js
+++ js/app.js
@@ -11,14 +11,16 @@ game.getRandomPhrase();
 game.handleInteraction();
 */
 
-
+/*
 // Checking results of Phrase class
-const phrase = new Phrase('hello sary are you here');
-phrase.addPhraseToDisplay('hello sary are you here');
-console.log(phrase.checkLetter('y', 'hello sary are you here'));
-console.log(phrase.checkLetter('z', 'hello sary are you here'));
+// const phrase = new Phrase('hello sary are you here');
+// phrase.addPhraseToDisplay('hello sary are you here');
+// console.log(phrase.checkLetter('y', 'hello sary are you here'));
+// console.log(phrase.checkLetter('z', 'hello sary are you here'));
 // phrase.showMatchedLetter('y', 'Sary are you here');
-
+*/
 
 const game = new Game();
+game.startGame();
 game.handleInteraction();
+
