9f0caac6bf62f76bd7a6c8e4dd80e1a42fc585ac
Sary Elmelegy <arch.selmelegy@gmail.com>
2018-11-13 15:11:25 +0200

    Phrase class creation

--- js/Phrase.js
+++ js/Phrase.js
@@ -3,5 +3,57 @@ class Phrase{
         this.phrase = phrase;
     }
 
-    addPhraseToDisplay(){}
+
+    /*
+     * Adds letter placeholders to the display when the game starts.
+     * @param   {string}   phrase - The phrase that needs to be hidden.
+     */
+    addPhraseToDisplay(phrase){
+        const phraseUL = $('#phrase ul')
+        for (let letter of phrase) {
+            if(letter !== ' '){
+                const listItem = document.createElement('li');
+                listItem.className = 'hide letter';
+                $(listItem).text(letter);
+                $(phraseUL).append(listItem);
+            } else if (letter === ' '){
+                const listItem = document.createElement('li');
+                listItem.className = 'hide space';
+                $(phraseUL).append(listItem);
+            }
+        }
+    }
+
+
+    /*
+     * Checks to see if letter selected by player matches a letter in the phrase.
+     * @param    {string}    letter - The letter users are searching for.
+     * @param    {string}    phrase - The phrase that needs to be searched.
+     * @return   {boolean}   true if the letter was found and false if not.
+     */
+    checkLetter(letter, phrase){
+        for (let i = 0; i < phrase.length; i++) {
+            if (phrase[i].toLowerCase() === letter.toLowerCase()) {
+                return true
+            }
+        }
+        return false
+    }
+
+
+    /*
+     * Reveals the letter(s) on the board that matches player's selection.
+     * @param    {string}    letter - The letter users are searching for.
+     * @param    {string}    phrase - The phrase that needs to be searched.
+     */
+    showMatchedLetter(letter, phrase){
+        const phraseUL = $('#phrase ul')
+        for (let i = 0; i < phrase.length; i++) {
+            if (phrase[i].toLowerCase() === letter.toLowerCase()) {
+                $(phraseUL).children().eq(i).removeClass('hide letter');
+                $(phraseUL).children().eq(i).addClass('show letter');
+            }
+        }
+    }
 }
+
--- js/app.js
+++ js/app.js
@@ -1,2 +1,14 @@
 const startDiv = $('#overlay');
 startDiv.hide();
+
+
+/*
+// Checking results of Phrase class
+
+const phrase = new Phrase('Sary are you here');
+phrase.addPhraseToDisplay('Sary are you here');
+console.log(phrase.checkLetter('y', 'Sary are you here'));
+console.log(phrase.checkLetter('z', 'Sary are you here'));
+phrase.showMatchedLetter('y', 'Sary are you here');
+*/
+
